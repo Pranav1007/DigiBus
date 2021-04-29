f1209c6f4a806d19da2d0943ffc3ac6751e5d955
Pranav1007 <pranav.b.kashyap@gmail.com>
2021-04-29 20:32:34 +0530

    Database Added

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '925709c46e120ecd46eb5aa502757785'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lqadnwzpkfpxik:10427c1c4db815184bbce51e127aa0cbfd6e6e1177e8b67275f682cdf65ce14c@ec2-52-21-252-142.compute-1.amazonaws.com:5432/d8gh3145k53ho9'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
